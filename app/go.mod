7dbccf0ed6d850e56bc951b398fcca7d54753615
Auxority <gide0013@hz.nl>
2023-10-30 11:37:00 +0100

    abc

--- app/go.mod
+++ app/go.mod
@@ -1,5 +1,3 @@
 module nsp/ais-receiver
 
 go 1.21.1
-
-require github.com/BertoldVdb/go-ais v0.1.0 // indirect
--- app/main.go
+++ app/main.go
@@ -1,30 +1,55 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
-	"strings"
 )
 
 func main() {
-	fmt.Println("Launching server...")
+	// Define the address and port for the server
+	address := "0.0.0.0" // Bind to all available network interfaces
+	port := "2001"       // Change this to your desired port number
 
-	// listen on all interfaces
-	ln, _ := net.Listen("tcp", ":2001")
+	// Create a TCP listener
+	listener, err := net.Listen("tcp", address+":"+port)
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		return
+	}
+	defer listener.Close()
 
-	// accept connection on port
-	conn, _ := ln.Accept()
+	fmt.Printf("AIS server is listening on %s:%s\n", address, port)
 
-	// run loop forever (or until ctrl-c)
 	for {
-		// will listen for message to process ending in newline (\n)
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		// output message received
-		fmt.Print("Message Received:", string(message))
-		// sample process for string received
-		newmessage := strings.ToUpper(message)
-		// send new string back to client
-		conn.Write([]byte(newmessage + "\n"))
+		// Accept incoming connections and start a goroutine to handle each connection
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	// Initialize a buffer to read data from the connection
+	buffer := make([]byte, 1024)
+
+	for {
+		// Read data from the connection into the buffer
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading from connection:", err)
+			return
+		}
+
+		// Process the received data (AIS messages) here
+		data := buffer[:n]
+		aisMessage := string(data)
+
+		fmt.Printf("Received AIS message: %s\n", aisMessage)
 	}
 }
