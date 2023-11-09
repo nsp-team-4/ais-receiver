ade1365b4b2696d03b1ac522c04fe063b93bbc1e
Auxority <gide0013@hz.nl>
2023-11-09 13:54:15 +0100

    update name

--- app/main.go
+++ app/main.go
@@ -13,7 +13,7 @@ import (
 // TODO: Replace with environment variables (and add these environment variables to the container instance)
 const (
 	connectionString = "Endpoint=sb://event-hubs-namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=AMM5XmxuGkvP1uJL9YYIF/3GlYjeAHU81+AEhMzqO8A="
-	eventHubName     = "raw-ais-data-eventhub"
+	eventHubName     = "ais-event-hub"
 )
 
 func main() {
