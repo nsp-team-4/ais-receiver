7ab0db618542dec6a3977ee4d042b3f3a4aa7c90
Auxority <gide0013@hz.nl>
2023-11-21 20:45:16 +0100

    first refactoring of tcp.go

--- app/tcp/tcp.go
+++ app/tcp/tcp.go
@@ -2,6 +2,7 @@ package tcp
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net"
 
@@ -9,46 +10,77 @@ import (
 )
 
 func RunServer() {
+	listener := createListener()
+	defer listener.Close()
+
 	log.Println("Server is running on port 2001")
 
+	for {
+		acceptAndHandleConnection(listener)
+	}
+}
+
+func createListener() net.Listener {
 	listener, err := net.Listen("tcp", ":2001")
 	if err != nil {
 		log.Println(err)
 	}
 
-	defer listener.Close()
-
-	for {
-		acceptConnection(listener)
-	}
+	return listener
 }
 
-func acceptConnection(listener net.Listener) {
-	conn, err := listener.Accept()
+func acceptAndHandleConnection(listener net.Listener) {
+	err := acceptConnection(listener)
 	if err != nil {
 		log.Println(err)
-		return
 	}
-
-	go handleConnection(conn)
 }
 
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-
-	log.Printf("Client connected %s", conn.RemoteAddr().String())
-
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		message := scanner.Text()
-		err := ais.HandleMessage(message)
-		if err != nil {
-			log.Println(err)
-		}
+func acceptConnection(listener net.Listener) error {
+	conn, err := listener.Accept()
+	if err != nil {
+		return fmt.Errorf("error accepting connection: %s", err)
 	}
 
+	go handleClient(conn)
+
+	return nil
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	log.Printf("Client connected %s", conn.RemoteAddr().String())
+	handleClientConnection(conn)
+}
+
+func handleClientConnection(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	processMessages(scanner)
+}
+
+func processMessages(scanner *bufio.Scanner) {
+	for scanner.Scan() {
+		processMessage(scanner)
+	}
+
+	handleScannerError(scanner)
+}
+
+func handleScannerError(scanner *bufio.Scanner) {
 	err := scanner.Err()
 	if err != nil {
 		log.Println("Error reading:", err)
+	}
+}
+
+func processMessage(scanner *bufio.Scanner) {
+	message := scanner.Text()
+	handleMessage(message)
+}
+
+func handleMessage(message string) {
+	err := ais.HandleMessage(message)
+	if err != nil {
+		log.Println(err)
 	}
 }
